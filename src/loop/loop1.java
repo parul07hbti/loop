0bfc33aeed1c4c33fec65c675ab617c86e7841f6
bn <parul07hbti>
2022-01-30 23:38:36 +0530

    Initial commit

---
+++ src/loop/loop1.java
@@ -0,0 +1,13 @@
+package loop;
+
+public class loop1 {
+    public static void main(String[] args) {
+        for(int i=0; i<=10;i++){
+            System.out.println("i love u"+i);
+        }
+        for (int i=10; i>=0; i--){
+            System.out.println("i love u"+i);
+        }
+
+    }
+}
---
+++ src/loop/loop2.java
@@ -0,0 +1,12 @@
+package loop;
+
+public class loop2 {
+    public static void main(String[] args) {
+        for (int i=0; i<=20; i++) {
+            int two = 2;
+            int output = two*i;
+            System.out.println(+i);
+            i++;
+        }
+    }
+}
